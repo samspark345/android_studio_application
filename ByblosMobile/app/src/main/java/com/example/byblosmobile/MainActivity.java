8be66dbf8e373f302d77cbf34bf8dbfb2675bd02
Zeyu Shao <zeyu.shao@hotmail.com>
2021-10-07 18:22:16 -0400

    linked signup and login activity with main activity

--- ByblosMobile/app/src/main/java/com/example/byblosmobile/MainActivity.java
+++ ByblosMobile/app/src/main/java/com/example/byblosmobile/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.byblosmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onSignup(View view){
+        Intent switchToSignup = new Intent(this, SignupActivity.class);
+        startActivity(switchToSignup);
+    }
+
+    public void onLogin(View view){
+        Intent switchToLogin = new Intent(this, LoginActivity.class);
+        startActivity(switchToLogin);
     }
 }
