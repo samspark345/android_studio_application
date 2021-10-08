4c74376634aaf96c22b1cecc23c23b72e721d88b
Selin Kararmaz <selinkararmaz1@gmail.com>
2021-10-08 14:46:42 +0300

    Made small changes to the sign up page and start page design, added onClick methods to sign up and login

--- ByblosMobile/app/src/main/java/com/example/byblosmobile/SignupActivity.java
+++ ByblosMobile/app/src/main/java/com/example/byblosmobile/SignupActivity.java
@@ -2,7 +2,9 @@ package com.example.byblosmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void onLogin(View view){
+        Intent switchToLogin = new Intent(this, LoginActivity.class);
+        startActivity(switchToLogin);
     }
 }
