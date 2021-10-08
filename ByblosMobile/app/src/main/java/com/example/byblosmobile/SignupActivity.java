64089e237acf285e0564c1af835f7f413d5a8894
Thulam Tran <ttran125@uottawa.ca>
2021-10-08 10:54:56 -0400

    Fixed radio buttons by using radioGroup

--- ByblosMobile/app/src/main/java/com/example/byblosmobile/SignupActivity.java
+++ ByblosMobile/app/src/main/java/com/example/byblosmobile/SignupActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -17,5 +18,22 @@ public class SignupActivity extends AppCompatActivity {
     public void onLogin(View view){
         Intent switchToLogin = new Intent(this, LoginActivity.class);
         startActivity(switchToLogin);
+    }
+
+    public void onRoleButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.customerRoleBtn:
+                if (checked)
+                    // customer role selected
+                    break;
+            case R.id.employeeRoleBtn:
+                if (checked)
+                    // employee role selected
+                    break;
+        }
     }
 }
