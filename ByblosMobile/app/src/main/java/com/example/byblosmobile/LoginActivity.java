d8e881732ace774313a8b8a7b8ceef488a382641
Zijun Ye <zijunye001@gmail.com>
2021-10-08 18:34:57 -0400

    edited login page

--- ByblosMobile/app/src/main/java/com/example/byblosmobile/LoginActivity.java
+++ ByblosMobile/app/src/main/java/com/example/byblosmobile/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.byblosmobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
-
+    private EditText role;
 
 
     @Override
@@ -24,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
 
         username = findViewById(R.id.loginName);
         password = findViewById(R.id.loginPassword);
+        //initialize role
+
     }
 
     private Boolean validateUsername(){
@@ -60,6 +67,46 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void checkAccount(){
+        String enteredUsername = username.getText().toString().trim();
+        String enteredPassword = password.getText().toString().trim();
+        String role = role.getText().toString();
+
+
+        if(role=="user"){
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("user");
+            Query checkUser = reference.orderByChild("username").equalTo(enteredUsername);
+            //event listener
+            checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String databasePassword = snapshot.child(enteredUsername).child("password").getValue(String.class);
+                        if (databasePassword.equals(enteredPassword)) {
+                            //sent name and role value to welcome page
+
+                        } else {
+                            password.setError("Wrong password!");
+                        }
+
+                    } else {
+                        username.setError("user account doesn't exist!");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            }
+        }
+        else if(role=="employee"){
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("employee");
+            //same above
+        }else{ // admin account
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("admin");
+            //just need check password
+
+        }
 
     }
 
