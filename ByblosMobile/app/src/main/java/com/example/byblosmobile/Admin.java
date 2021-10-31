6c421344770f97cf6ad3be92ee255535ef51473f
Selin Kararmaz <selinkararmaz1@gmail.com>
2021-10-31 22:48:50 +0300

    Added branchDatabase class and created addBranch deleteBranch methods for admin

--- ByblosMobile/app/src/main/java/com/example/byblosmobile/Admin.java
+++ ByblosMobile/app/src/main/java/com/example/byblosmobile/Admin.java
@@ -1,10 +1,27 @@
 package com.example.byblosmobile;
 
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
+
+
 
 public class Admin extends AppCompatActivity {
 
     ArrayList<Service> servicesList = new ArrayList<Service>(50);
+    ListView servicesListView = findViewById(R.id.servicesListView);
+    BranchDatabase branchDatabaseBase = new BranchDatabase(this);
+
+
+
+    public void addBranch(String branch){
+        branchDatabaseBase.addBranch(branch);
+
+    }
+
+    public boolean deleteBranch(String branch){
+        return branchDatabaseBase.deleteBranch(branch);
+    }
 
 }
